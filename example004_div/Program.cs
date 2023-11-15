25254c7d092608183ae0ae9afcab79c8cb886d0a
Ruslan <ruslan.leonoff2014@yandex.ru>
2023-11-15 18:21:01 +0300

    new change

--- example004_div/Program.cs
+++ example004_div/Program.cs
@@ -1,2 +1,3 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿double numberA = 42;
+double numberB = 23;
+Console.WriteLine (numberA / numberB);
