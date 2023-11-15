49d8b875268668bdc6ff73160ef4d07ecc33df1e
Ruslan <ruslan.leonoff2014@yandex.ru>
2023-11-15 20:01:14 +0300

    Задача с нахождением максимального чилса

--- example006_base/Program.cs
+++ example006_base/Program.cs
@@ -1,1 +1,14 @@
-﻿
+﻿int a = 1;
+int b = 2;
+int c = 6;
+int e = 4;
+
+
+int max = a;
+if (a> max ) max= a;
+if (b > max) max = b;
+if (c> max) max = c;
+if (e> max ) max= e;
+
+Console.WriteLine ("max=");
+Console.WriteLine (max);
